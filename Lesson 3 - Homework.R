bef2657572abbe399fecbee7d8a4c65b0ff9a2a4
steffenredeker <steffen_redeker@posteo.de>
2023-05-02 17:40:51 +0200

    Lesson 3 - Homework

---
+++ Lesson 3 - Homework.R
@@ -0,0 +1,35 @@
+rm(list = ls())
+
+library(readxl)
+
+data_mpc <- read_excel("C:/Users/redek/Desktop/R/Data visualization with R/lesson 3/data_mpc.xlsx", 
+                       skip = 1)
+View(data_mpc)
+
+data_mpc$country
+
+countries <- unique(data_mpc$country)
+
+cols <- c("blue", "red", "orange", "black", "purple")
+
+#grid of plots
+par(mfrow = c(2, 3))
+
+for (i in 1:length(countries)) {
+  
+  income_germany <- data_mpc$income[data_mpc$country == countries [i]]
+  
+  consumption_germany <- data_mpc$consumption[data_mpc$country == countries [i]]
+  
+  consumption_share <- (consumption_germany/income_germany)*100
+  
+  plot(1991:2022,
+       consumption_share,
+       main = countries[i],
+       xlab = "years",
+       ylab = "%",
+       type = "l")
+}
+
+  
+  
