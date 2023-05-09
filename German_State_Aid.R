42ab5a589121f448d0f60f18a8e15969611c46d7
steffenredeker <steffen_redeker@posteo.de>
2023-05-09 19:13:34 +0200

    German State Aid

--- German_State_Aid.R
+++ German_State_Aid.R
@@ -4,7 +4,9 @@ colnames(awards)
 
 germany_awards <- awards[awards$member_state == "Germany", ]
 
-germany_awards$year_month <- format(as.Date(paste0(germany_awards$publication_year, "-", germany_awards$publication_month, "-01")), "%Y-%m")
+sum_by_year_month <- format(as.Date(paste0(germany_awards$publication_year, "-", germany_awards$publication_month, "-01")), "%Y-%m")
+
+
 
 plot(sum_by_year_month$raw_amount, type = "p",
      xlab = "Year-Month", ylab = "Raw Amount", main = "Germany - State Aid", xaxt="n")
@@ -20,3 +22,4 @@ axis(1, at = seq_along(sum_by_year_month$year_month)[c(1,13,25,37,49)],
      labels = c("2016", "2017", "2018", "2019", "2020"), tick=TRUE, col.axis="black")
 
 legend("topleft", legend = c("Raw Amount", "Regression Line", "Median Line"), col = c("black", "blue", "red"), lty = c(1, 1, 2), cex = 0.8)
+
