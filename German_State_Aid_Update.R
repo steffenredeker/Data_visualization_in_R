2078f1da52636656c522dcd05082f6277bf6a2d6
steffenredeker <steffen_redeker@posteo.de>
2023-05-09 21:30:03 +0200

    Update - new Y Axis

--- German_State_Aid_Update.R
+++ German_State_Aid_Update.R
@@ -1,6 +1,10 @@
+# Introduction ----
 library(eusa)
+library(scales)
 data("awards")
 colnames(awards)
+
+# Main plot part ----
 
 germany_awards <- awards[awards$member_state == "Germany", ]
 
@@ -9,16 +13,27 @@ germany_awards$year_month <- format(as.Date(paste0(germany_awards$publication_ye
 sum_by_year_month <- aggregate(raw_amount ~ year_month, data = germany_awards, FUN = sum)
 
 plot(sum_by_year_month$raw_amount, type = "p",
-     xlab = "Year-Month", ylab = "Raw Amount", main = "Germany - State Aid", xaxt="n")
+     xlab = "Year-Month", ylab = "Raw Amount", main = "Germany - State Aid", xaxt="n", yaxt="n")
 
+# Regression line ----
 reg_model <- lm(raw_amount ~ year_month, data = germany_awards)
+abline(a = coef(reg_model)[1], b = coef(reg_model)[2], col = "blue")
 
-abline(reg_model, col = "blue")
-
+# Median line ----
 median_by_year_month <- aggregate(raw_amount ~ year_month, data = germany_awards, FUN = median)
 abline(h = median_by_year_month$raw_amount, col = "red", lty = 2)
 
+# Axis & Legend ----
 axis(1, at = seq_along(sum_by_year_month$year_month)[c(1,13,25,37,49)], 
      labels = c("2016", "2017", "2018", "2019", "2020"), tick=TRUE, col.axis="black")
 
+axis(2, at = c(1e9, 2e9, 3e9, 4e9, 5e9), 
+     labels = c("1 bn.", "2 bn.", "3 bn.", "4 bn.", "5 bn."), tick=TRUE, col.axis="black")
+
 legend("topleft", legend = c("Raw Amount", "Regression Line", "Median Line"), col = c("black", "blue", "red"), lty = c(1, 1, 2), cex = 0.8)
+
+# Problem ----
+# Median and regression line seem to refer to the whole data set, but not to the modified plot with monthly sum values.
+# However, when I change the data to "sum_by_year_month" it's not working either.
+
+
