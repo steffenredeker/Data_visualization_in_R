5a7a94c2eb8215ba3cfe4f8fb42e939c590b8cd7
steffenredeker <steffen_redeker@posteo.de>
2023-06-09 11:22:25 +0200

    Update

--- Homeworks/Homework Lesson 8.R
+++ Homeworks/Homework Lesson 8.R
@@ -27,22 +27,8 @@ life_expectancy_column <- "lifeExp"
 china_life_expectancy_by_year <- china_data[c("year", life_expectancy_column)]
 print(china_life_expectancy_by_year)
   
-# plot1 - colors but no legend
-                                 
-ggplot() +
-  geom_line(data = maximum, aes(x = year, y = gapminder), color = "blue", linetype = "dashed") +
-  geom_line(data = median, aes(x = year, y = gapminder), color = "orange", linetype = "dotted") +
-  geom_line(data = minimum, aes(x = year, y = gapminder), color = "red") +
-  geom_line(data = china_life_expectancy_by_year, aes(x = year, y = lifeExp), color = "black", linetype = "solid") +
-  labs(x = " ", y = "Life expectancy at birth", color = " ") +
-  scale_color_manual(values = c("blue", "orange", "red", "black"),
-                     labels = c("Maximum","Median", "Minimum", "China")) +
-  theme_minimal() +
-  theme(panel.border = element_rect(color = "black", fill = NA)) +
-  theme(legend.position = "bottom")
-
-# plot2 - legend but wrong colors
-
+# plot1
+                                
 ggplot() +
   geom_line(data = maximum, aes(x = year, y = gapminder, color = "Maximum"), linetype = "dashed") +
   geom_line(data = median, aes(x = year, y = gapminder, color = "Median"), linetype = "dotted") +
